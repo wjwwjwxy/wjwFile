056f1e5c95dd86b4294682d8a6c9e7081152c3f7
jarvisjwwu <jarvisjwwu@tencent.com>
2021-04-12 17:46:12 +0800

    修改webpack

--- wjw/markdown/常用工具函数或者工具类.js
+++ wjw/markdown/常用工具函数或者工具类.js
@@ -30,3 +30,28 @@ function getGCD(a=16,b=4){
   }
   return getGCD(b,a % b)
 }
+
+/*
+深拷贝
+ */
+function deepClone(obj) {
+  // 如果是 值类型 或 null，则直接return
+  if(typeof obj !== 'object' || obj === null) {
+    return obj
+  }
+  // 定义结果对象
+  let copy = {}
+  // 如果对象是数组，则定义结果数组
+  if(obj.constructor === Array) {
+    copy = []
+  }
+  // 遍历对象的key
+  for(let key in obj) {
+    // 如果key是对象的自有属性
+    if(obj.hasOwnProperty(key)) {
+      // 递归调用深拷贝方法
+      copy[key] = deepClone(obj[key])
+    }
+  }
+  return copy
+}
